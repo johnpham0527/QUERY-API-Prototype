e97862532730a64a90524fbfe14af4026609a9b3
drewhsu86 <bowserhsu@gmail.com>
2020-05-31 17:28:14 -0400

    seeding one library and one program worked

--- seed/index.js
+++ seed/index.js
@@ -1,15 +1,21 @@
 const db = require("../db")
+const mongoose = require("mongoose")
 const Library = require("../models/libraries")
 const Presenter = require("../models/presenters")
 const Program = require("../models/programs")
 
 db.on("error", console.error.bind(console, "MongoDB Connection Error:"))
 
+// made an id to do ObjectId in programs
+const LIB1ID = new mongoose.Types.ObjectId
+
+// create libraries using the schema add them 
 const libraries = async () => {
   try {
     await Library.deleteMany()
 
     const libraries = [{
+      _id: LIB1ID,
       name: "Queens Public Library at Forest Hills",
       address: "108-19 71st Ave, Forest Hills, NY 11375",
       programs: []
@@ -23,8 +29,42 @@ const libraries = async () => {
   }
 }
 
+// create programs using the schema and add them 
+const programs = async () => {
+  try {
+    await Program.deleteMany()
+
+    const programs = [{
+      // ways to define date 
+      // new Date('December 17, 1995 03:24:00')
+      // new Date('1995-12-17T03:24:00')
+      // new Date(1995, 11, 17, 3, 24, 0) (months go from 0 to 11 )
+      startTime: new Date(2019, 11, 20, 4, 0, 0),
+      duration: 2,
+      // normally the name needs to be grabbed via library id 
+      // this ID is not valid and will not populate 
+      library: LIB1ID,
+      libraryName: "Queens Public Library at Forest Hills",
+      category: "Book Drive",
+      attendees: {
+        childrenAttendees: 5,
+        teenAttendees: 21,
+        adultAttendees: 12,
+        seniorAttendees: 6
+      }
+    }]
+
+    await Program.insertMany(programs)
+    console.log("Seeded initial programs!")
+
+  } catch (er) {
+    console.log(er)
+  }
+}
+
 const run = async () => {
   await libraries()
+  await programs()
   db.close()
 }
 
