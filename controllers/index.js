b734db397108dc92a849f129fb562fffd649a392
drewhsu86 <bowserhsu@gmail.com>
2020-05-31 19:03:37 -0400

    successfully posted a program that also updates the librarys array of programs

--- controllers/index.js
+++ controllers/index.js
@@ -1,6 +1,7 @@
 const db = require("../db")
 const Library = require("../models/libraries")
 const Program = require("../models/programs")
+const Presenter = require("../models/presenters")
 require("dotenv").config()
 
 db.on("error", console.error.bind(console, "MongoDB Connection Error:"))
@@ -19,6 +20,17 @@ async function getLibraries(req, res) {
     })
 
     res.json(libNames)
+  } catch (error) {
+    res.status(500).json({ error: error.message })
+  }
+}
+
+// Get all presenters
+async function getPresenters(req, res) {
+  try {
+    const presenters = await Presenter.find()
+
+    res.json(presenters)
   } catch (error) {
     res.status(500).json({ error: error.message })
   }
@@ -46,12 +58,81 @@ async function getLibraryPrograms(req, res) {
       programs.push(prog)
     }
 
+    // we can limit the number of results 
+    const limit = req.query.limit
+
+    try {
+      if (limit) {
+        programs = programs.slice(0, parseInt(limit))
+      }
+    } catch (er) {
+      console.log(er)
+    }
+
     res.json(programs)
   } catch (error) {
     res.status(500).json({ error: error.message })
   }
 }
 
+// Get all programs for a library
+// currently copy pasted from getLibraryPrograms
+// so theres a chance to efficiently DRY up redundant code 
+async function getLibraryProgramsId(req, res) {
+  try {
+    const libId = req.params.libraryId
+
+    // use regex to do case insensitive find 
+    const libraries = await Library.findById(libId)
+
+    // we would do this with .populate() but I wasn't able to populate 
+    // object ids in an array so we will have to do it the hard way 
+    const programs = []
+    for (let i = 0; i < libraries.programs.length; i++) {
+      const prog = await Program.find({ "_id": libraries.programs[i] })
+      programs.push(prog)
+    }
+
+    // we can limit the number of results 
+    const limit = req.query.limit
+
+    try {
+      if (limit) {
+        programs = programs.slice(0, parseInt(limit))
+      }
+    } catch (er) {
+      console.log(er)
+    }
+
+    res.json(programs)
+  } catch (error) {
+    res.status(500).json({ error: error.message })
+  }
+}
+
+// post a program 
+// need to have an object with all the right schema values in the post body
+async function postProgram(req, res) {
+  try {
+    const newPost = req.body
+    // because json cannot transmit a javascript date 
+    // we will input an array [Y, M, D, H, M, S]
+    newPost.startTime = new Date(...newPost.startTime)
+    const program = await new Program(newPost)
+    await program.save()
+
+    // we also want to link this in the schema of the library that hosts it
+    const library = await Library.findById(program.library)
+    library.programs.push(program['_id'])
+    await library.save()
+
+    res.json(program)
+  } catch (error) {
+    res.status(500).json({ error: error.message })
+  }
+}
+
 module.exports = {
-  getLibraries, getLibraryPrograms
+  getLibraries, getLibraryPrograms, getLibraryProgramsId,
+  postProgram, getPresenters
 }
--- routes/index.js
+++ routes/index.js
@@ -11,8 +11,19 @@ router.get("/", (req, res) => {
 // get all libraries 
 router.get("/libraries", (req, res) => controllers.getLibraries(req, res))
 
+// get all presenters 
+router.get("/presenters", (req, res) => controllers.getPresenters(req, res))
+
 // get all programs based on library name 
 // can do case insensitive and spaces replaced by dashes or underscores 
-router.get("/libraries/:libraryName", (req, res) => controllers.getLibraryPrograms(req, res))
+// can take limit as a query "/libraries/name/:libraryName?limit=2"
+router.get("/libraries/name/:libraryName", (req, res) => controllers.getLibraryPrograms(req, res))
+
+// get all programs based on library id 
+// can take limit as a query "/libraries/name/:libraryName?limit=2"
+router.get("/libraries/id/:libraryId", (req, res) => controllers.getLibraryProgramsId(req, res))
+
+// post a new program 
+router.post("/libraries/new", (req, res) => controllers.postProgram(req, res))
 
 module.exports = router
