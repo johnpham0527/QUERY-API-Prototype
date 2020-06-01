25d8126cc3856cb7e25f9d39a6292e72d6365ef6
John Pham <mr.john.pham@gmail.com>
2020-06-01 06:31:46 -0400

    Use static middleware

--- app.js
+++ app.js
@@ -3,9 +3,13 @@ const bodyParser = require("body-parser")
 const cors = require("cors")
 const logger = require("morgan")
 const routes = require("./routes")
+const path = require("path")
 
 const app = express()
 
+express.static("/")
+
+app.use(express.static(path.join(__dirname, 'public')))
 app.use(cors())
 app.use(bodyParser.json())
 app.use(logger("dev"))
